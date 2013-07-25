c11cf0f20ec15cb1f5214229a92bd4f5384885b3
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-25 00:17:10 -0500

    added validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ActiveRecord::Base
+  validates :organizer_name, presence: true
+  validates :organizer_email, presence: true
+  validates :title, presence: true
+  validates :date, presence: true, format: {with: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/}
 end
